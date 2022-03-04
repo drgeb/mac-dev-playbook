9b7608fa4cd343696a47302cb1fef01b5ab7f20c
Gerry Bennett <gerry.bennett@ninjaone.com>
2022-03-03 20:52:27 -0600

    oh no baby I am sorry forgot to add these roles

---
+++ roles/drgeb.outlook/files/set_default_outlook.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from LaunchServices import *
+
+LSSetDefaultHandlerForURLScheme("mailto", "com.microsoft.outlook")
+LSSetDefaultRoleHandlerForContentType("com.apple.ical.ics", kLSRolesAll, "com.microsoft.outlook")
+LSSetDefaultRoleHandlerForContentType("public.vcard", kLSRolesAll, "com.microsoft.outlook")
+LSSetDefaultRoleHandlerForContentType("com.apple.mail.email", kLSRolesAll, "com.microsoft.outlook")
+
+#  OutlookDefault.py
+#  
+#
